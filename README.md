6e5f78a7e2bea9cda05234cc2bf99e3a349c0d95
Sean Rütschi <bitlog@users.noreply.github.com>
2025-05-29 09:16:51 +0200

    added reddit.user.js script, version 0.1

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # userscripts
+
+A collection of JavaScript userscripts intended for daily usage with a browser extension such as Tampermonkey
+
+1. [reddit.user.js](reddit.user.js) *redirect reddit to old interface*
---
+++ reddit.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name	reddit
+// @description	redirect reddit to old interface
+// @version	0.1
+// @author	bitlog
+// @namespace	bitlogUserscripts
+// @updateURL	https://github.com/bitlog/userscripts/blob/main/reddit.user.js
+// @match	*://*.reddit.com/*
+// @match	*://preview.redd.it/*
+// @run-at	document-start
+// @grant	none
+// ==/UserScript==
+
+if (location.hostname === "preview.redd.it") {
+	location.replace(location.protocol + "//i.redd.it" + location.pathname);
+} else if (location.hostname !== "old.reddit.com" && location.pathname.indexOf('/poll/') !== 0 && location.pathname.indexOf('/gallery/') !== 0) {
+	var oldReddit = location.protocol + "//old.reddit.com" + location.pathname + location.search + location.hash;
+	location.replace(oldReddit);
+}

d619d0c22edead0d1f52fb58133a552b864ae576
Sean Rütschi <bitlog@users.noreply.github.com>
2025-06-13 10:56:37 +0200

    updated youtube.user.js to version 0.11

--- youtube.user.js
+++ youtube.user.js
@@ -1,21 +1,30 @@
 // ==UserScript==
 // @name	youtube
-// @description	redirect youtube shorts
-// @version	0.10
+// @description	youtube userscript, including youtube shorts redirect
+// @version	0.11
 // @author	bitlog
 // @namespace	bitlogUserscripts
 // @downloadURL	https://raw.githubusercontent.com/bitlog/userscripts/refs/heads/main/youtube.user.js
 // @updateURL	https://raw.githubusercontent.com/bitlog/userscripts/refs/heads/main/youtube.user.js
 // @match	*://*.youtube.com/*
 // @run-at	document-idle
-// @grant	none
+// @grant	bitlogStyle
 // ==/UserScript==
 
 function shortsRedirectChecker() {
 	if (location.pathname.match(/^\/shorts\//i)) {
-		alert("shorts page detected, redirecting...");
 		location.replace(location.protocol + "//" + location.host + location.pathname.replace(/^\/shorts\/\/watch?v=/i);
 	}
 	setTimeout(shortsRedirectChecker, 500);
 }
 shortsRedirectChecker();
+
+bitlogStyle(`
+/* Remove shorts thumbnail in homepage and search */
+[is-shorts], [aria-label="Shorts"], #guide-button, [tab-title="Shorts"] {
+	display: none;
+}
+ytd-reel-shelf-renderer {
+	display: none;
+}
+`)

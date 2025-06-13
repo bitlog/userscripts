9a3ae0bb1b1510448d184de0b8a4fe004c62470e
Sean Rütschi <bitlog@users.noreply.github.com>
2025-06-13 10:04:20 +0200

    updated youtube.user.js to version 0.8

--- youtube.user.js
+++ youtube.user.js
@@ -1,23 +1,14 @@
 // ==UserScript==
 // @name	youtube
-// @description	redirect youtube
-// @version	0.7
+// @description	redirect youtube shorts
+// @version	0.8
 // @author	bitlog
 // @namespace	bitlogUserscripts
 // @downloadURL	https://raw.githubusercontent.com/bitlog/userscripts/refs/heads/main/youtube.user.js
 // @updateURL	https://raw.githubusercontent.com/bitlog/userscripts/refs/heads/main/youtube.user.js
-// @match	*://*.youtube.com/*
+// @match	*://*.youtube.com/shorts/*
 // @run-at	document-idle
 // @grant	none
 // ==/UserScript==
 
-window.document.addEventListener("yt-navigate-start", redirectYoutubeShorts(), false);
-window.document.addEventListener("yt-navigate-finish", redirectYoutubeShorts(), false);
-redirectYoutubeShorts();
-
-
-function redirectYoutubeShorts() {
-	if (window.location.pathname.match(/^\/shorts\//i)) {
-		window.location.replace(window.location.protocol + "//" + window.location.host + window.location.pathname.replace(/^\/shorts\/\/watch?v=/i);
-	}
-}
+window.location.replace(window.location.protocol + "//" + window.location.host + window.location.pathname.replace(/^\/shorts\/\/watch?v=/i);

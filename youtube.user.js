61199e59966f6b639e8600b99d18b140333cfa7f
Sean Rütschi <bitlog@users.noreply.github.com>
2025-06-13 09:14:32 +0200

    updated youtube.user.js to version 0.3

--- youtube.user.js
+++ youtube.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name	youtube
 // @description	redirect youtube
-// @version	0.2
+// @version	0.3
 // @author	bitlog
 // @namespace	bitlogUserscripts
 // @downloadURL	https://raw.githubusercontent.com/bitlog/userscripts/refs/heads/main/youtube.user.js
 // @updateURL	https://raw.githubusercontent.com/bitlog/userscripts/refs/heads/main/youtube.user.js
 // @match	*://*.youtube.com/*
-// @run-at	document-start
+// @run-at	document-idle
 // @grant	none
 // ==/UserScript==
 

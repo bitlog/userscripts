ab69e19e5a2f0e0a112265e0c9316b458543ec9d
Sean Rütschi <bitlog@users.noreply.github.com>
2025-06-13 10:21:00 +0200

    updated reddit.user.js to version 0.11

--- reddit.user.js
+++ reddit.user.js
@@ -1,19 +1,18 @@
 // ==UserScript==
 // @name	reddit
 // @description	redirect reddit to old interface
-// @version	0.10
+// @version	0.11
 // @author	bitlog
 // @namespace	bitlogUserscripts
 // @downloadURL	https://raw.githubusercontent.com/bitlog/userscripts/refs/heads/main/reddit.user.js
 // @updateURL	https://raw.githubusercontent.com/bitlog/userscripts/refs/heads/main/reddit.user.js
 // @match	*://*.reddit.com/*
-// @exclude	*://www.reddit.com/media*
 // @match	*://*.redd.it/*
 // @run-at	document-start
 // @grant	none
 // ==/UserScript==
 
-if (location.hostname === "www.reddit.com") {
+if (location.hostname === "www.reddit.com" && !location.pathname.match(/^\/media/i)) {
 	location.replace(location.protocol + "//old.reddit.com" + location.pathname + location.search + location.hash);
 } else if (location.hostname === "preview.redd.it") {
 	location.replace(location.protocol + "//i.redd.it" + location.pathname);

8c7e8b1bd990b128070a198f0c739096b90d5c8e
Sean Rütschi <bitlog@users.noreply.github.com>
2025-08-08 15:22:26 +0200

    updated youtube-disable-autoplay.user.js to version 2025.08.08.02

--- youtube-disable-autoplay.user.js
+++ youtube-disable-autoplay.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name	youtube-disable-autoplay
 // @description	disable youtube auto-play
-// @version	2025.08.08.01
+// @version	2025.08.08.02
 // @author	bitlog
 // @namespace	bitlogUserscripts
 // @downloadURL	https://raw.githubusercontent.com/bitlog/userscripts/refs/heads/main/youtube-disable-autoplay.user.js
@@ -11,12 +11,12 @@
 // @grant	bitlogStyle
 // ==/UserScript==
 
-var var_interval_id = window.setInterval( function( window ) {
-	let a = window.document.querySelector( "button.ytp-button[data-tooltip-target-id='ytp-autonav-toggle-button']" ); // get the button
-	if ( a.getAttribute( "aria-label" ) == "Autoplay is on" ) {
-		a.click( ); // disable autoplay next if enabled
+var disableAutoPlay = window.setInterval( function( window ) {
+	let button = window.document.querySelector( "button.ytm-autonav-toggle-button-container" ); // get the button
+	if ( button.getAttribute( "aria-label" ) == "Auto-play is on" ) {
+		button.click( ); // disable autoplay next if enabled
 	}
-	if ( a.getAttribute( "aria-label" ) == "Autoplay is off" ) {
-		window.clearInterval( var_interval_id ); // end script once done
+	if ( button.getAttribute( "aria-label" ) == "Auto-play is off" ) {
+		window.clearInterval( disableAutoPlay ); // end script once done
 	}
 }, 1024, window );
